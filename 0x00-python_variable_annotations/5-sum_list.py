af1e47ba55720326e98063d5e34289c87ffcbc4d
Precious Peter <peterprecious370@gmail.com>
2024-06-06 05:15:11 -0700

    sum list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''a module for complex annotation'''
+
+
+def sum_list(input_list: [float, ...]) -> float:
+    '''function that sums a list'''
+    return float(sum(input_list))
