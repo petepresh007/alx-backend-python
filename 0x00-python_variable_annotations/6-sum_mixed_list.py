c6fc594ce17ca8a7e0de4cbc67631f01bdf748cf
Precious Peter <peterprecious370@gmail.com>
2024-06-06 05:37:09 -0700

    mixed list fixed

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''module for a list with mixed values'''
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: [int, float]) -> float:
-    return float(sum(mxd_lst))
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    return sum(mxd_lst)
