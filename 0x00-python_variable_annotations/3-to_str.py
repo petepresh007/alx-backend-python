a3a0651ddecd04553aac77ec6fe09590b2876a1e
Precious Peter <peterprecious370@gmail.com>
2024-06-06 04:48:10 -0700

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''a module with a function that returns a str from a float'''
+
+
+def to_str(n: float) -> str:
+    '''a function that returns a str from a float'''
+    return str(n)
