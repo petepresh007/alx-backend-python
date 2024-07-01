60ceec52fe0249e52bf26aa8ad46fc1f77ae9bbd
peter precious <peteprecious370@gmail.com>
2024-07-01 11:05:57 -0700

    second to last

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -73,6 +73,15 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_public.assert_called_once()
             mock_json.assert_called_once()
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """ unit-test for GithubOrgClient.has_license """
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
