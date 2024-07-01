50aab91b06ac0b2155012f983645ddae0daff467
peter precious <peteprecious370@gmail.com>
2024-07-01 10:01:36 -0700

    fourth task

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -49,6 +49,35 @@ class TestGetJson(unittest.TestCase):
 
             # Assert that the function's output is equal to test_payload
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(
+                TestClass,
+                'a_method',
+                return_value=42
+                ) as mocked_method:
+            test_instance = TestClass()
+            # Call a_property twice
+            result_first_call = test_instance.a_property
+            result_second_call = test_instance.a_property
+
+            # Assert the mocked a_method was called exactly once
+            mocked_method.assert_called_once()
+
+            # Assert the results of both calls are the same and correct
+            self.assertEqual(result_first_call, 42)
+            self.assertEqual(result_second_call, 42)
 
 
 if __name__ == "__main__":
