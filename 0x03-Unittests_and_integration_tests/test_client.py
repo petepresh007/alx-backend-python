53594d3963ac8e4b3cf3726d92e3feb735370546
peter precious <peteprecious370@gmail.com>
2024-07-01 10:58:40 -0700

    4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -51,6 +51,28 @@ class TestGithubOrgClient(unittest.TestCase):
             # Assert that _public_repos_url returns the expected URL
             self.assertEqual(client._public_repos_url, expected_url)
 
+    @patch('client.get_json')
+    def test_public_repos(self, mock_json):
+        """
+        Test that the list of repos is what you expect from the chosen payload.
+        Test that the mocked property and the mocked get_json was called once.
+        """
+        json_payload = [{"name": "Google"}, {"name": "Twitter"}]
+        mock_json.return_value = json_payload
+
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_public:
+
+            mock_public.return_value = "hello/world"
+            test_class = GithubOrgClient('test')
+            result = test_class.public_repos()
+
+            check = [i["name"] for i in json_payload]
+            self.assertEqual(result, check)
+
+            mock_public.assert_called_once()
+            mock_json.assert_called_once()
+
 
 if __name__ == "__main__":
     unittest.main()
