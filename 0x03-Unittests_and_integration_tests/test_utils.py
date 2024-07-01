f0daeb8a4ac328fabaf33b6b125aeb7acb687772
peter precious <peteprecious370@gmail.com>
2024-07-01 09:23:37 -0700

    fixed error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,18 +16,14 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-        ({}, ("a",), "a"),
-        ({"a": 1}, ("a", "b"), "b"),
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
     ])
-    def test_access_nested_map_exception(
-            self,
-            nested_map,
-            path,
-            expected_exception_message
-            ):
-        with self.assertRaises(KeyError) as context:
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """ Test that a KeyError is raised for the respective inputs """
+        with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), expected_exception_message)
+        self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
 
 
 if __name__ == "__main__":
