9b357c12836623d3c35d13c88532ac485d880c3b
peter precious <peteprecious370@gmail.com>
2024-07-01 10:48:15 -0700

    mocking

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''a module for client test'''
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 from utils import get_json
@@ -28,6 +28,28 @@ class TestGithubOrgClient(unittest.TestCase):
 
         # Assert that the result is correct
         self.assertEqual(result, {"login": "sample_org"})
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    '''a class to mock'''
+    @parameterized.expand([
+        ("google", "https://api.github.com/orgs/google/repos"),
+        ("abc", "https://api.github.com/orgs/abc/repos"),
+    ])
+    def test_public_repos_url(self, org_name, expected_url):
+        '''methods with parameters'''
+        with patch(
+                'client.GithubOrgClient.org',
+                new_callable=PropertyMock
+                ) as mock_org:
+            # Mock the org property to return a payload with the expected URL
+            mock_org.return_value = {"repos_url": expected_url}
+
+            # Instantiate the GithubOrgClient with the org_name
+            client = GithubOrgClient(org_name)
+
+            # Assert that _public_repos_url returns the expected URL
+            self.assertEqual(client._public_repos_url, expected_url)
 
 
 if __name__ == "__main__":
